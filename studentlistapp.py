8976e499b4a3fde8fc77b78e1524b227c6cd8ddc
MattiasFrisk <72252855+MattiasFrisk@users.noreply.github.com>
2020-10-02 11:32:36 +0200

    Update studentlistapp.py
    Gör uppgiften tillsammans med Joshua, ifall att det bara syns 1 pull request från oss så är det därför.

--- studentlistapp.py
+++ studentlistapp.py
@@ -1,4 +1,4 @@
-studentlist = ["xxyy0011", "joso1122"]
+studentlist = ["xxyy0011", "joso1122", "frma18iy", "cajo18qe"]
 
 print("These students have finished the lab: ")
 for student in studentlist:
