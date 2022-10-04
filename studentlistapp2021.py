ae34f675f96c758de30eb12440366fc9169a4177
isse20uv <112944725+isse20uv@users.noreply.github.com>
2022-10-04 13:29:25 +0200

    Update studentlistapp2021.py

--- studentlistapp2021.py
+++ studentlistapp2021.py
@@ -1,4 +1,4 @@
-studentlist = ["xxyy0011", "joso1122", "johson", "ljuvik"]
+studentlist = ["xxyy0011", "isse20uv", "joso1122", "johson", "ljuvik"]
 
 print("These students have finished the lab: ")
 for student in studentlist:
