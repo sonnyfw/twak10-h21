db724d7e91579ddf23415be7745685bf345097ed
Daddelfjun <peos19zm@student.ju.se>
2022-10-04 09:59:30 +0200

    Update studentlistapp2021.py

--- studentlistapp2021.py
+++ studentlistapp2021.py
@@ -1,4 +1,4 @@
-studentlist = ["xxyy0011", "joso1122", "johson", "ljuvik"]
+studentlist = ["xxyy0011", "joso1122", "johson", "ljuvik", "peos19zm", "beel20wz"]
 
 print("These students have finished the lab: ")
 for student in studentlist:
